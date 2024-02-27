88d6e1d3b99c15bd19b4811393197a37f6dacb02
MaminProgramistGromozda <159258248+MaminProgramistGromozda@users.noreply.github.com>
2024-02-27 12:12:41 +0300

    Лаба 1 1-3 лвл

---
+++ lvl 2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+struct Sessia
+{
+    private int _pred1;
+    private int _pred2;
+    private int _pred3;
+    private int _pred4;
+    private string _name;
+    private double _sr;
+    public int Pred1
+    {
+        get { return _pred1; }
+        set { _pred1 = value; }
+    }
+    public int Pred2
+    {
+        get { return _pred2; }
+        set { _pred2 = value; }
+    }
+    public int Pred3
+    {
+        get { return _pred3; }
+        set { _pred3 = value; }
+    }
+    public int Pred4
+    {
+        get { return _pred4; }
+        set { _pred4 = value; }
+    }
+    public double Sredn
+    {
+        get { return _sr; }
+        set { _sr = value; }
+    }
+    public Sessia(string nm, int p1, int p2, int p3, int p4)
+    {
+        _pred1 = p1;
+        _pred2 = p2;
+        _pred3 = p3;
+        _pred4 = p4;
+        _name = nm;
+        _sr = getsred();
+    }
+    private double getsred()
+    {
+        return (_pred1 + _pred2 + _pred3 + _pred4) / 4;
+    }
+    public void Print()
+    {
+        if (_sr >= 4)
+        {
+            Console.WriteLine(_name + ": " + _pred1 + " " + _pred2 + " " + _pred3 + " " + _pred4 + " Sred: " + _sr);
+        }
+        else
+        {
+            Console.WriteLine(_name + ": " + _pred1 + " " + _pred2 + " " + _pred3 + " " + _pred4 + " Sred: " + _sr + " Не прошел по баллам");
+        }
+        
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Sessia smert = new Sessia("Иванов1", 4, 3, 5, 4);
+        Sessia[] ocenki = new Sessia[]
+        {
+            new Sessia("Иванов1", 4, 5, 3, 4),
+            new Sessia("Иванов2", 5, 5, 5, 3),
+            new Sessia("Иванов3", 3, 3, 5, 2),
+            new Sessia("Иванов4", 5, 5, 5, 5),
+        };
+        Sort(ocenki);
+        foreach(Sessia rezultati in ocenki)
+        {
+            rezultati.Print();
+        }
+    }
+    static void Sort(Sessia[] ocenki)
+    {
+        int i = 0;
+        int j = 1;
+        while(j < ocenki.Length - 1)
+        {
+            if (i < 0 || ocenki[i].Sredn >= ocenki[i + 1].Sredn)
+            {
+                i = j;
+                j++;
+            }
+            while (i >= 0 && ocenki[i].Sredn < ocenki[i + 1].Sredn)
+            {
+                Sessia temp = ocenki[i];
+                ocenki[i] = ocenki[i + 1];
+                ocenki[i + 1] = temp;
+                i--;
+            }
+        }
+    }
+}
---
+++ lvl 3.cs
@@ -0,0 +1,86 @@
+﻿using System;
+struct Football
+{
+    private string _teams;
+    private bool _score;
+
+    public bool Score
+    {
+        get { return _score; }
+        set { _score = value; }
+    }
+    public Football(string tm, bool sc)
+    {
+        _teams = tm;
+        _score = sc;
+    }
+    public void Print()
+    {
+        if (_score == false)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(_teams + ": " + "Поражение" + " " + "Команда проиграла и не прошла в следующий этап");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(_teams + ": " + "Победа" + " " + "Поздравляем! Команда прошла в следующий этап!");
+            Console.ResetColor();
+        }
+    }
+    public void WinPrint()
+    {
+        if (_score == true)
+        {
+            Console.WriteLine(_teams + ": " + "Прошли в следующий тур");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Football Tims = new Football("Команда0", true);
+        Football[] komandi = new Football[]
+        {
+            new Football("Команда1", false),
+            new Football("Команда2", true),
+            new Football("Команда3", false),
+            new Football("Команда4", false),
+            new Football("Команда5", true),
+            new Football("Команда6", true),
+        };
+        Sort(komandi);
+        Console.WriteLine("Сетка определяется следующим образом. Каждая команда играет с противоположной по счету: 1-12, 2-11, 3-10, 4-9, 5-8, 6-7. Если рядом с командой стоит 0 - команда проиграла и не прошла. Если 1 - команда вышла во второй тур");
+        foreach(Football rezultati in komandi)
+        {
+            rezultati.Print();
+        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Список прошедших команд:");
+        Console.ResetColor();
+        foreach (Football rezultati in komandi)
+        {
+            rezultati.WinPrint();
+        }
+    }
+    static void Sort(Football[] komandi)
+    {
+        int i = 1;
+        while (i < komandi.Length)
+        {
+            if (i == 0 || komandi[i - 1].Score || !komandi[i].Score)
+            {
+                i++;
+            }
+            else
+            {
+                Football temp = komandi[i - 1];
+                komandi[i - 1] = komandi[i];
+                komandi[i] = temp;
+                i--;
+            }
+        }
+    }
+}
